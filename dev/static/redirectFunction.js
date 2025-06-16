179ca5325d2b97e7c635b4085d7df21a41131244
Harshit <kartiklohani2004@gmail.com>
2025-06-16 17:49:34 +0530

    some ui additions

--- dev/static/redirectFunction.js
+++ dev/static/redirectFunction.js
@@ -7,10 +7,18 @@ function renderPlanner(){
 
 document.addEventListener("DOMContentLoaded", function () {
     const button = document.getElementById("signInButton");
-    console.log("Found the sign in button");
     if (button) {
         button.addEventListener("click", function () {
             window.location.href = "/auth/signup";
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("recommenderLink");
+    if (button) {
+        button.addEventListener("click", function () {
+            window.location.href = "/recommender";
         });
     }
 });
